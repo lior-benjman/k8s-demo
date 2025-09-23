ac519ffb26640755432ad5b342f8835193ac23e2
lior-benjman <lior114452@gmail.com>
2025-09-23 17:37:37 +0300

    scripting files

---
+++ k8s/deploy.sh
@@ -0,0 +1,23 @@
+set -e
+
+NAME="kubernetes-demo-api"
+USERNAME="lior114452"
+IMAGE="$USERNAME/$NAME:0.1.0"
+
+echo "Building Docker image..."
+docker build -t $IMAGE .
+
+echo "Pushing Docker image to Docker Hub..."
+docker push $IMAGE
+
+echo "Applying Kubernetes deployment..."
+kubectl apply -f k8s/deployment.yaml
+kubectl apply -f k8s/service.yaml
+
+echo "Getting pods..."
+kubectl get pods
+echo "Getting services..."
+kubectl get services
+
+echo "Fetching the main service"
+kubectl get services $NAME
